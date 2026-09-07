04284fa88176930a0e2ffd475979fa289710169e
Vincent Velociter <vincent.velociter@gmail.com>
2026-09-07 12:31:02 -0500

    Update example app android config

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -1,9 +1,6 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // The Flutter Gradle Plugin must be applied after the Android Gradle Plugin.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -30,12 +27,6 @@ android {
             // Signed with the debug keys, so that `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
     }
 }
 
--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -19,6 +19,10 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.2.0" apply false
+    // Declared but not applied: this only pins the Kotlin Gradle Plugin on the build
+    // classpath, which is where AGP's built-in Kotlin picks its compiler up. AGP 9.2.0
+    // would otherwise bring Kotlin 2.2.10, below the 2.2.20 the Flutter Gradle plugin
+    // requires.
     id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
 
